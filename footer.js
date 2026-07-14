e28f2114417e1d26aa4402a06d50bfab86811ca6
lynnsrecipes <mccowan2812@gmail.com>
2026-07-14 14:23:53 -0400

    Change email address in Footer class
    Updated email address in footer component.

--- footer.js
+++ footer.js
@@ -2,7 +2,7 @@ class Footer extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         	<div style="height:50px";"></div>
-		<div class="footer" style="text-align: center;">by <a href="mailto:jcm21884@gmail.com?subject=Lynn's%20Recipes%20PDF%20version">JC McCowan</a> created 10/8/2025 v.2026.07.14</div>
+		<div class="footer" style="text-align: center;">by <a href="mailto:mccowan2812@gmail.com?subject=Lynn's%20Recipes%20PDF%20version">JC McCowan</a> created 10/8/2025 v.2026.07.14</div>
 		<div style="height:50px";></div>
 
         `;
