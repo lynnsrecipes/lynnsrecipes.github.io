2ec69a5de55b3562813c999d2ccbd166d59f0447
lynnsrecipes <mccowan2812@gmail.com>
2026-07-14 14:56:17 -0400

    Update footer content and formatting

--- footer.js
+++ footer.js
@@ -3,7 +3,8 @@ class Footer extends HTMLElement {
         this.innerHTML = `
         	<div style="height:50px";"></div>
 		<div class="footer" style="text-align: center;"><a href="mailto:mccowan2812@gmail.com?subject=Lynn's%20Recipes%20PDF%20version">Click Here</a> to report an issue or broken link.</div>
-		<div class="footer" style="text-align: center;">by JC McCowan created 10/8/2025 v.2026.07.14</div>
+		<div class="footer" style="text-align: center;">Created by JC McCowan</div>
+		<div class="footer" style="text-align: center;">10/8/2025 v.2026.07.14</div>
 		<div style="height:50px";></div>
         `;
     }
