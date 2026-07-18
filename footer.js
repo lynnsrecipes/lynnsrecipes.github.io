33baf06f0cd25bca18fcd8c7b0e8d626d100423a
lynnsrecipes <mccowan2812@gmail.com>
2026-07-18 12:48:47 -0400

    Modify footer HTML and update version date
    Updated footer content with new version date.

--- footer.js
+++ footer.js
@@ -7,7 +7,7 @@ const containerfooter = document.getElementById("content-footer");
         	<div style="height:10px";"></div>
 		<div class="footer" style="text-align: center;"><a href="mailto:mccowan2812@gmail.com?subject=Lynn's%20Recipes%20PDF%20version">Click Here</a> to report an issue or broken link.</div>
 		<div class="footer" style="text-align: center;">Created by JC McCowan</div>
-		<div class="footer" style="text-align: center;">10/8/2025 v.2026.07.17</div>
+		<div class="footer" style="text-align: center;">10/8/2025 v.2026.07.18</div>
 		<div style="height:50px";></div>
         `;
 
